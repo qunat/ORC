864741fc1174ebe00c0320ccbe1331a00b387f81
loujiand <304817449@qq.com>
2021-11-10 00:03:33 +0800

    第二次更新

--- OCR.py
+++ OCR.py
@@ -49,21 +49,50 @@ class baidu_ocr(object):
                         self.key_str=self.key_str+i["words"]
                     else:
                         self.key_str = self.key_str +","+ i["words"]
-                if self.enter_count==0:
-                    with open("./key.dat","w") as f:
-                        f.writelines(self.key_str)
-                    self.enter_count+=1
-                    self.key_str=""
-                else:
-                    with open("./value.dat","w") as f:
-                        f.writelines(self.key_str)
-                    self.enter_count+=1
-                    self.key_str = ""
-                if self.enter_count==2:
+
+                with open("./value.dat", "w") as f:
+                    f.writelines(self.key_str)
+                self.enter_count += 1
+                self.key_str = ""
+
+                if True:
                     self.enter_count=0
                     keyboard.wait(hotkey="alt+ctrl+e")  # 等待键盘的触发。等待QQ截图
                     time.sleep(0.1)
                     self.orc_join()
+
+    def Get_key(self):
+        while True:
+            # 截图
+            keyboard.wait(hotkey="alt+ctrl+a")  # 等待键盘的触发。等待QQ截图
+            keyboard.wait(hotkey="enter")
+            time.sleep(0.1)
+
+            # 图片保存
+            image = ImageGrab.grabclipboard()  # 获取剪切板的图片
+            image.save('screen.jpg')  # 图片保存
+
+            # 打开screen.jpg图片，以read bytes方式进行操作
+            with open('screen.jpg', 'rb') as f:
+                image = f.read()  # image就是这张图片的二进制内容
+                text = self.client.basicAccurate(image)  # 调用百度的接口帮我们识别图片的内容
+                #print(text)
+                result = text['words_result']  # 获取返回内容的字典
+                # 遍历输出字典内容
+                for i in result:
+                    self.key_list.append(i["words"])
+                    if self.key_str=="":
+                        self.key_str=self.key_str+i["words"]
+                    else:
+                        self.key_str = self.key_str +","+ i["words"]
+                if self.enter_count==0:
+                    with open("./key.dat","w") as f:
+                        f.writelines(self.key_str)
+                        f.close()
+                    self.enter_count+=1
+                    self.key_str=""
+                    break
+
     def orc_join(self):
         dict={}
         with open("./value.dat", "r") as f:
@@ -105,4 +134,5 @@ def pytesseract_ocr():
 
 #pytesseract_ocr()
 new=baidu_ocr()
+new.Get_key()
 new.baidu_ocr()
